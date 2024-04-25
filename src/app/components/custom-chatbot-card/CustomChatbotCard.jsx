1297e3ee7df2ce47c10b6894ab38b8a425bcfcf2
archana <archanak@appscrip.co>
2024-04-25 14:16:42 +0530

    feat: added customChatbotCards

---
+++ src/app/components/custom-chatbot-card/CustomChatbotCard.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image"
+
+const CustomChatbotCard = (props) => {
+    return (
+        <div className=" flex flex-col items-center flex-1 p-7  gap-2">
+           <Image src={props.image} height={320} width={320} className="xl:w-full 2xl:h-full 2xl:w-full"/>
+           <span className="text-xl 2xl:text-4xl font-bold w-80 2xl:w-full text-center">{props.title}</span>
+           <p className="text-base 2xl:text-xl font-normal w-80 2xl:w-full text-center">{props.subtitle}</p>
+        </div>
+    )
+}
+
+export default CustomChatbotCard
--- src/app/page.js
+++ src/app/page.js
@@ -4,15 +4,28 @@ import BuiltInTouchpoint from "../../public/assets/logos/built in touchpoint.web
 import ModuleCard from "./components/module-card/ModuleCard";
 import CustomChatbotCard from "./components/custom-chatbot-card/CustomChatbotCard";
 import fiveminchatbot from "../../public/assets/images/make ai chatbot in 5 steps.webp"
+import customizeUI from "../../public/assets/images/customize AI chatbot.webp"
+import gainCustomer from "../../public/assets/images/gain customer.webp"
+
 
 export default function Home() {
   return (
-    <main className="flex flex-col md:flex-row items-center justify-between p-5">
+    <main className="flex flex-col md:flex-row items-center justify-between gap-2">
       
   <CustomChatbotCard
   image = {fiveminchatbot}
   title="Make an AI chatbot in 5 simple steps" 
   subtitle ="Follow our straightforward onboarding flow to create and deploy an AI chatbot on your website."
+  />
+   <CustomChatbotCard
+  image = {customizeUI}
+  title="Customize your AI chatbot UI" 
+  subtitle ="Tailor every customer interaction and deliver unpatalleled AI customer service using your first-party data."
+  />
+   <CustomChatbotCard
+  image = {gainCustomer}
+  title="Gain customer insights" 
+  subtitle ="Refine your services, products, and business strategies with critical customer data from your AI chatbot conversations."
   />
     </main>
   );
